1bef0925730e2e1837bcd005c6111b7f9e46b530
Bakeforfun <Baker@bakeforfun.local>
2016-07-04 21:34:32 +0300

    Fixed permissions

--- develop/2016-07-04-bff-logistic-regression.py
+++ develop/2016-07-04-bff-logistic-regression.py
@@ -63,6 +63,7 @@ filename = '/logs/lr_logloss.txt'
 dir = os.path.dirname(filename)
 if not os.path.exists(dir):
     os.makedirs(dir)
+    os.chmod(dir, 0755)
 
 f = open(os.getcwd() + '/logs/lr_logloss.txt', 'w')
 f.write('This is a test\n')
@@ -87,5 +88,6 @@ filename = '/output/lr_submit.csv'
 dir = os.path.dirname(filename)
 if not os.path.exists(dir):
     os.makedirs(dir)
+    os.chmod(dir, 0755)
 
 lr_submit.to_csv(os.getcwd() + '/output/lr_submit.csv')
